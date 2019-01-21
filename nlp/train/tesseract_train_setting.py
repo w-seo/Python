5fa3222c5b737ab1ae9ce6b3670b7d7ef2258100
Murcielago <44044609+Woobin-Seo@users.noreply.github.com>
2019-01-21 16:06:24 +0900

    Update tesseract_train_setting.py

--- nlp/train/tesseract_train_setting.py
+++ nlp/train/tesseract_train_setting.py
@@ -1,7 +1,7 @@
-TRAIN_IMAGE_FILE_PATH = "C:\\Users\\woobi\\opencv\\images\\court_rulling_0.png"
+TRAIN_IMAGE_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\images\\ファイル名.png"
 
-TIFF_IMAGE_FILE_PATH = "C:\\Users\\woobi\\opencv\\nlp\\images\\tiff_images\\srk_tif.tif"
+TIFF_IMAGE_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\images\\tiff_images\\srk_tif.tif"
 
 TRAIN_TIFF_IMAGE_FILE_NAME = "tesseract_tiff"
 
-ROI_IMAGE_FILE_PATH = "C:\\Users\\woobi\\opencv\\nlp\\images\\roi_images\\"
+ROI_IMAGE_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\images\\roi_images\\"
