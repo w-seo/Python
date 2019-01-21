b7eea5edcc42af3bcf4322072ec04b8677e6468d
Murcielago <44044609+Woobin-Seo@users.noreply.github.com>
2019-01-21 16:05:11 +0900

    Update setting.py

--- nlp/setting.py
+++ nlp/setting.py
@@ -1,16 +1,16 @@
-RECOGNITION_IMAGE_FILE_PATH = "C:\\Users\\woobi\\opencv\\nlp\\recog_images\\"
-IMAGE_FILE_PATH = "C:\\Users\\woobi\\opencv\\images\\"
+RECOGNITION_IMAGE_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\recog_images\\"
+IMAGE_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\images\\"
 
-FILE_NAME = "Court_ruling.pdf"
-PDF_FILE_PATH = "C:\\Users\\woobi\\opencv\\pdf\\"
+FILE_NAME = "ファイル名.pdf"
+PDF_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\pdf\\"
 
-IMG_ZIP_PATH = "C:\\Users\\woobi\\opencv\\nlp\\images\\images_zip\\"
-IMG_UNZIP_PATH = "C:\\Users\\woobi\\opencv\\nlp\\images\\images_unzip\\"
+IMG_ZIP_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\images\\images_zip\\"
+IMG_UNZIP_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\images\\images_unzip\\"
 
 # 物体認識からの結果物
 JSON_CONFIG_FILE_NAME = "json_config_Court_ruling.json"
-JSON_CONFIG_FILE_PATH = "C:\\Users\\woobi\\opencv\\nlp\\json_config\\"
+JSON_CONFIG_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\json_config\\"
 # ocrからの結果物
-OCR_CSV_FILE_PATH = "C:\\Users\\woobi\\opencv\\nlp\\ocr_csv_file\\"
+OCR_CSV_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\ocr_csv_file\\"
 # 自然言語処理からの結果物
-NLP_RESULT_CSV_FILE_PATH = "C:\\Users\\woobi\\opencv\\nlp\\nlp_csv_file\\"
+NLP_RESULT_CSV_FILE_PATH = "C:\\Users\\ユーザー名\\opencv\\nlp\\nlp_csv_file\\"
